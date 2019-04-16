ee8f0845421aeec5b12caa8a2f7b5e2601862fc5
Inu Lin <inu.lin@shopee.com>
2019-04-16 22:40:34 +0800

    Updates

---
+++ precache-manifest.ff2e8ea7194f0bf65b55d9cff1546ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26d92f36fffce00e69ca",
+    "url": "/love-and-producer-cost-counter/static/js/runtime~main.fcbacba2.js"
+  },
+  {
+    "revision": "81681eee8b9cd70d7c21",
+    "url": "/love-and-producer-cost-counter/static/js/main.6e659052.chunk.js"
+  },
+  {
+    "revision": "05eab4076f48178f97fa",
+    "url": "/love-and-producer-cost-counter/static/js/2.7337812d.chunk.js"
+  },
+  {
+    "revision": "81681eee8b9cd70d7c21",
+    "url": "/love-and-producer-cost-counter/static/css/main.ba1ce448.chunk.css"
+  },
+  {
+    "revision": "05eab4076f48178f97fa",
+    "url": "/love-and-producer-cost-counter/static/css/2.3fec388c.chunk.css"
+  },
+  {
+    "revision": "9047b9a73df8717d269e8045cca3c89c",
+    "url": "/love-and-producer-cost-counter/index.html"
+  }
+];
