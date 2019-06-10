d0a5e0a147845c3057f47529a2d82d75fce94bc2
Inu Lin <inu.lin@shopee.com>
2019-06-10 21:23:46 +0800

    Updates

---
+++ precache-manifest.24dc650942d6d0eff744f1406f1837a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26d92f36fffce00e69ca",
+    "url": "/love-and-producer-cost-counter/static/js/runtime~main.fcbacba2.js"
+  },
+  {
+    "revision": "47256177c8863304fa92",
+    "url": "/love-and-producer-cost-counter/static/js/main.08260646.chunk.js"
+  },
+  {
+    "revision": "ce581e0ca48e7402403e",
+    "url": "/love-and-producer-cost-counter/static/js/2.451374c3.chunk.js"
+  },
+  {
+    "revision": "47256177c8863304fa92",
+    "url": "/love-and-producer-cost-counter/static/css/main.74ae9e84.chunk.css"
+  },
+  {
+    "revision": "ce581e0ca48e7402403e",
+    "url": "/love-and-producer-cost-counter/static/css/2.3fec388c.chunk.css"
+  },
+  {
+    "revision": "938ed6f867175c578db57e83cdd73e64",
+    "url": "/love-and-producer-cost-counter/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/love-and-producer-cost-counter/precache-manifest.88e96946dbe9fd65966c75750a1022fb.js"
+  "/love-and-producer-cost-counter/precache-manifest.24dc650942d6d0eff744f1406f1837a0.js"
 );
 
 workbox.clientsClaim();
